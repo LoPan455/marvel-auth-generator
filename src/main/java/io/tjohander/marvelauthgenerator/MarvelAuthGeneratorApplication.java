edcb46da349b614eeea3886417ed50e45aed7333
lopan455 <tjohander@gmail.com>
2021-05-18 08:41:40 -0500

    Initial Commit

---
+++ src/main/java/io/tjohander/marvelauthgenerator/MarvelAuthGeneratorApplication.java
@@ -0,0 +1,14 @@
+package io.tjohander.marvelauthgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelAuthGeneratorApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MarvelAuthGeneratorApplication.class, args);
+    }
+
+}
